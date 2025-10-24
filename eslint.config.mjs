ff99c4db7a436f39d235b753d5bf2a9b070ca8f9
Leo Jacquet <leojacquet2003@gmail.com>
2025-10-24 18:46:10 +0200

    fix ESLint ignores and add lint script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig([
         plugins: { js },
         extends: ['js/recommended'],
         languageOptions: { globals: globals.browser },
-        ignores: ['server/', 'platforms/'],
+        ignores: ['server/*', 'platforms/*'],
         rules: {
             'no-unused-vars': 'error',
             'no-var': 'error',
